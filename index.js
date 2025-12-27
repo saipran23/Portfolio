72d7ca4acec9746c3e443039954889be3f80cce5
Alchuri Sai Praneeth Reddy <praneethalchuri@gmail.com>
2025-12-27 17:15:14 +0530

    After add the exprees.js to the project

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+app.listen(port, () => {
+  console.log(`Portfolio app running at http://localhost:${port}`);
+});
